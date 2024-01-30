d63d663b9bc1fc48dc3eb1e94c9d53b4d7d13cd3
Jclarinha <joaoclarinha13@gmail.com>
2024-01-30 11:55:57 +0000

    minor change

--- obj/Debug/net8.0/TesteGIT.AssemblyInfo.cs
+++ obj/Debug/net8.0/TesteGIT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TesteGIT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3605c72cf47efbe197397915ce5a18b8639a6206")]
 [assembly: System.Reflection.AssemblyProductAttribute("TesteGIT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TesteGIT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
